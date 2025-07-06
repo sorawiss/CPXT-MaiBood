337d1741271db5f08013c75b87fb1681b134cefa
sorawiss <sorawit.bunnee@gmail.com>
2025-07-06 08:00:22 +0700

    Fix  FridgeList component by adding additional properties to FridgeItem and improving error handling; update FridgeItems interface for better type safety

--- src/app/fridge/components/FridgeList.tsx
+++ src/app/fridge/components/FridgeList.tsx
@@ -3,22 +3,35 @@
 interface FridgeItem {
     id: string;
     name: string;
+    amount: number;
+    status: 'fresh' | 'selling' | 'sold' | 'expired';
+    user_id: string;
+    created_at: Date;
+    updated_at: Date;
 }
 
 interface FridgeItems {
-    items: FridgeItem[];
-    error?: string;
+    success?: string;
+    items?: FridgeItem[];
+    error?: string | { message: string; error: unknown };
 }
 
 function FridgeList({ fridgeItems }: { fridgeItems: FridgeItems }) {
     console.log("fridgeItems", fridgeItems)
     return (
         <div>
-            {fridgeItems.items.map((item: FridgeItem) => (
+            {fridgeItems.items && fridgeItems.items.map((item: FridgeItem) => (
                 <div key={item.id}>{item.name}</div>
             ))}
 
-            {fridgeItems.error && <div>{fridgeItems.error}</div>}
+            {fridgeItems.error && (
+                <div>
+                    {typeof fridgeItems.error === 'string' 
+                        ? fridgeItems.error 
+                        : fridgeItems.error.message
+                    }
+                </div>
+            )}
         </div>
     )
 }
