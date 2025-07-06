3d021158068f10ad105fe8a4b559859399536b27
sorawiss <sorawit.bunnee@gmail.com>
2025-07-06 07:17:40 +0700

    Style : loading effect on button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,16 +1,21 @@
+import { LoaderCircle } from "lucide-react";
+
+
+
 interface ButtonProps {
     type: "button" | "submit" | "reset";
     text: string;
     onClick?: () => void;
     className?: string;
+    isLoading?: boolean;
 }
 
-function Button({ type, text, onClick, className }: ButtonProps) {
+function Button({ type, text, onClick, className, isLoading }: ButtonProps) {
   return (
     <button 
       type={type} onClick={onClick}
-      className={`bg-makro text-white rounded-[38px] h-[3rem] w-full ${className}`}
-    >{text}</button>
+      className={`bg-makro text-white rounded-[38px] h-[3rem] w-full flex items-center justify-center cursor-pointer ${className}`}
+    >{isLoading ? <LoaderCircle className="animate-spin" /> : text}</button>
   )
 }
 export default Button
