72ee1959272afa3bb8b1e9c2e7a8e74f7f749ea3
sorawiss <sorawit.bunnee@gmail.com>
2025-07-06 15:48:27 +0700

    Feat : ui for category

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import UserLocation from "./components/UserLocation";
 
 import Search from "./components/Searh";
+import Category from "./components/Category";
 
 
 export default function Home() {
 
-  
+
   return (
     <div className="flex flex-col items-center justify-center pt-[4rem] w-[25rem] px-[1.5rem] " >
       <div className="UserLocation w-full bg-makro rounded-2xl px-[1rem] py-[0.5rem]
@@ -14,10 +15,17 @@ export default function Home() {
           <h3>มีส้มลดราคาเป็นจำนวนมากในชุมชน</h3>
           <h2 className="text-textprimary" >ลองเข้าไปดูสิ</h2>
         </div>
+
         <UserLocation />
+
       </div>
 
-      <Search />
+      <div className="SearchAndCategory w-full flex flex-col gap-[1rem] mt-[2rem] ">
+        <Search />
+        <Category />
+      </div>
+
+
     </div>
   );
 }
