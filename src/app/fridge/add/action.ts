4576caa52d1e36f86b6bd14bd70569905337473f
sorawiss <sorawit.bunnee@gmail.com>
2025-07-09 17:18:21 +0700

    Feat: revalidate fridge path after adding item

--- src/app/fridge/add/action.ts
+++ src/app/fridge/add/action.ts
@@ -1,6 +1,7 @@
 "use server";
 import { verifySession } from "@/utils/session";
 import { addToFridge } from "@/utils/DALs";
+import { revalidatePath } from "next/cache";
 
 export async function handleAddToFridge(formData: FormData) {
   const session = await verifySession();
@@ -21,6 +22,8 @@ export async function handleAddToFridge(formData: FormData) {
     new Date(expiry_date as string),
     session.userId as string
   );
+
+  revalidatePath("/fridge")
 }
 
 
